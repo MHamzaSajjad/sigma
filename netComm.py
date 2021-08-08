3015467d5a56d14921c14f97f9b4b8b2d29f0094
atom501 <60687754+atom501@users.noreply.github.com>
2021-08-08 09:49:14 +0500

    Add files via upload

--- netComm.py
+++ netComm.py
@@ -17,6 +17,42 @@ class Client:
         self.public_key = pubKey
         self.private_key = PrivKey
 
+    def del_from_file(self, itemName, quantity): #add new item or decrease old items quantity and write back updated dictionary
+        items_loaded = self.load_store()
+
+        if(itemName in items_loaded):
+            temp_tup = items_loaded[itemName]
+
+            final_quantity = temp_tup[1] - quantity
+            if(final_quantity <= 0):
+                del items_loaded[itemName]
+
+            else:
+                items_loaded[itemName] = (temp_tup[0], final_quantity, temp_tup[2])
+        
+        string_to_write = json.dumps(items_loaded)
+
+        file = open("Store.txt", "w")
+        file.write(string_to_write)
+
+
+    #reads dictionary of format {itemName:(publicKey, quantity, price) ...}
+    def load_store(self): #used to load the dictionary in Store.txt
+        file = open("Store.txt", "r")
+        items = file.read()
+        items = json.loads(items)
+
+        return items
+
+    #reads dictionary of format {portNumber:(N, E) ...}  
+    def load_keys(self): #loads key dictionary for use
+        file = open("keys.txt", "r")
+        keys = file.read()
+        keys = json.loads(keys)
+
+        return keys
+
+
     def menu(self):
 
         while True:
@@ -28,35 +64,66 @@ class Client:
             if(option == 1): #displays all the items in the Store.txt file
                 file = open("Store.txt", "r")
 
-                item_info = file.readline()
+                item_info = file.read()  #reads dictionary of format {itemName:(publicKey, quantity, price) ...}
 
-                while(item_info != ""):
-                    print(item_info)
+                item_info = json.loads(item_info)
 
-                    item_info = file.readline()
-                
-                file.close()
+                for key in item_info.keys():
+                    tup = item_info[key]
+                    print("Item name:", key, "quatity available:", tup[1], "price:", tup[2], "seller public key:", tup[0])
 
                 print("")
             
-            elif(option == 2): #adds to the Store.txt file
+            elif(option == 2): #adds to the Store.txt file assuming two people don't sell same item
 
                 item = input("Enter the item name: ")
                 price = input("Enter item price: ")
+                quantity = input("Enter item quantity: ")
                 publicKey = input("Enter your public key")
 
-                item_data = str(item) + " " + str(price) + " " + str(publicKey)
+                items_loaded = self.load_store()
 
-                file = open("Store.txt", "a")
-                file.write(item_data)
+                if item in items_loaded: #if already exists only increase quantity
+                    temp_tup = items_loaded[item]
 
-                file.close()
+                    items_loaded[item] = (temp_tup[0], temp_tup[1] + quantity, temp_tup[2])
+
+                else: #if doesn't exists create entry
+                    items_loaded[item] = (publicKey, quantity, price)
+
+
 
             
-            #elif(option == 3): #tells the seller of the transaction and adds it to the currently unconfirmed transactions list
+            elif(option == 3): #tells the seller of the transaction and adds it to the currently unconfirmed transactions list
+                
+                money_in_wallet = get_from_hash() #need to be added (optional idea)
 
+                item = input("Enter item you want to buy: ")
+                quantity = input("Enter the quantity: ")
 
+                items_loaded = self.load_store()
 
+                if(not(item in items_loaded)): #if item does not exists
+                    print("That item is not found\n")
+                    continue
+
+                temp_tup = items_loaded[item]
+
+                item_price = temp_tup[2]
+                item_quantity = temp_tup[1]
+
+                if(money_in_wallet >= (item_price * quantity)):
+                    #tell the seller you want to buy
+
+                    #recieve confirmation from seller
+
+                    #both use each other's public keys decode a message sent with private key to confirm identity
+
+                    #create data struct for blockchain or add to unconfirm transactions
+
+                    #add item to blockchain by itself or just wait for miner to do it
+
+                    #message seller item was sold
 
 
     def receive_handler(self):
@@ -78,22 +145,22 @@ if __name__ == "__main__":
     # creats public private key pairs
     # and adds the public key to the open list in keys.txt
 
-    
-
-
     (pubkey, privkey) = rsa.newkeys(256)  #creates public private key pair
     user = Client(userName, pubkey, privkey)
 
-    pubN = pubkey.n
+    pubN = pubkey.n  #seperated as rsa.PublicKey is not JSON serialisable
     pubE = pubkey.e
 
-    pubkey_add = str(pubN) + " " + str(pubE) + " " + user.port #saves public key and port to a file
+    keys_loaded = user.load_keys()
 
-    file = open("keys.txt", "a")
-    file.write(str_pubkey)
+    keys_loaded[user.port] = (pubN, pubE)
+
+    file = open("keys.txt", "w")
+    keys_string = json.dumps(keys_loaded)
+    file.write(keys_string)
     file.close()
 
-    print(user.sock)
+    #print(user.sock)
     try:
         # Start receiving Messages
         T = Thread(target=user.receive_handler)
