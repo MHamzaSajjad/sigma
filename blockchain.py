b57c17342f78032c0bfeb2b6d0053395166df3a9
Muhammad Hamza Sajjad <54877353+creakyman@users.noreply.github.com>
2021-08-08 13:59:04 +0500

    Add files via upload

---
+++ blockchain.py
@@ -0,0 +1,148 @@
+# You do NOT need to import any other libraries
+import hashlib
+from datetime import datetime
+import time
+import pickle
+
+class Block:
+    # The constructor initializes all the components of a block
+    def __init__(self, data, previous_block_hash = ''):
+        self.timestamp = datetime.now().strftime("%d/%m/%Y") # Timestamps add a factor of verification when checking a block's validity
+        self.nonce = 0 # This adds randomization to the mix when the block is mined
+        self.data = data #1 block contains 1 transaction
+        self.previous_block_hash = previous_block_hash
+        self.hash = self.calculate_hash()
+
+    # Returns the SHA256 hash of a concatenated string containing the nonce, timestamp, data and previous block's hash (in the same order). Remember to encoded
+    # the string before hashing it!
+    def calculate_hash(self):
+        # YOUR CODE HERE
+        concat_string = str(self.nonce) + self.timestamp + self.data + self.previous_block_hash #concatenating as given above
+        hash_string = hashlib.sha256(concat_string.encode('utf-8')).hexdigest() #encoding the concatenating string in utf-8, then hashing it in sha256
+        return hash_string
+
+    # Requires a miner to provide a proof of work before they could modify the blockchain
+    def mine_block(self, difficulty):
+        # YOUR CODE HERE
+        check = False
+        while not check: #loop will keep running until 'proof of work' is fulfilled (number of zeroes at start == difficulty)
+            num_zeroes_start = 0
+            for i in range(difficulty): #checking if the first difficulty number of characters in hash string are zeroes or not
+                if(self.hash[i] == '0'):
+                    num_zeroes_start += 1
+                else:
+                    break
+            if(num_zeroes_start == difficulty): #if number of zeroes at start are equal to difficulty, okay, else generate hash again and check again
+                check = True
+            else:
+                self.nonce += 1 #increment nonce
+                self.hash = self.calculate_hash() #calculate hash again (mining)
+
+def open_chain_from_file():
+    file = open("blockchain.txt", "r")
+    i = 0
+    chain = Blockchain(1, "")
+    for line in file.readlines():
+        line = line.rstrip('\n')
+        if i == 0:
+            diff = int(line)
+            chain.difficulty = diff
+            i += 1
+        else:
+            line = line[1:-2]
+            var_list = line.split()
+            # 
+            # var_list = []
+            # string_var = ""
+            # c = 0
+            # for charac in line:
+            #     if c == 3:
+            #         c += 1
+            #         continue
+            #     elif c == 4:
+            #         c = 0
+            #         continue
+            #     elif c == 2:
+            #         var_list.append(string_var)
+            #         string_var = ""
+            #         c += 1
+            #         continue
+            #     if charac == "'":
+            #         c += 1
+            #         continue
+            #     string_var += charac
+            print(var_list)
+
+            new_block = Block(var_list[2], var_list[3])
+            new_block.timestamp = var_list[0]
+            new_block.nonce = int(var_list[1])
+            new_block.hash = var_list[4]
+            chain.add_block(new_block)
+    return chain
+
+
+class Blockchain:
+    # The constructor initializes a chain containing just the genesis block
+    def __init__(self, difficulty, genesis_block_data):
+        self.difficulty = difficulty
+        self.chain = [self.create_genesis_block(genesis_block_data)]
+
+    # Initializes and returns the first block in a chain
+    def create_genesis_block(self, data):
+        # YOUR CODE HERE
+        genesis_block = Block(data) #initialise genesis block, default value of previous block hash already set to ''
+        return genesis_block
+
+    # Adds a new block to the end of the chain. Remember blocks need to be mined before they can be added!
+    def add_block(self, block):
+        # YOUR CODE HERE
+        block.mine_block(self.difficulty) #mine the block to see if proof of work is provided
+        self.chain.append(block) #then append to the blockchain once its mined successfully (according to given difficulty)
+
+    # Returns the block that was most recently added
+    def get_latest_block(self):
+        # YOUR CODE HERE
+        return self.chain[-1] #-1 index gives the last block in the list (latest added block)
+
+    # Returns a bool which indicates whether the chain is still valid or have been tampered with
+    def is_chain_valid(self):
+        # YOUR CODE HERE
+        genesis_block = self.chain[0] #getting first block from chain as genesis block
+        calc_hash = genesis_block.calculate_hash() #checking genesis block's hash
+        if(calc_hash != genesis_block.hash): #if calculated hash does not match stored hash, return False
+            return False
+        calc_prev_hash = calc_hash #storing in prev hash for next comparison
+        for block in self.chain[1:]: #checking after genesis block
+            calc_hash = block.calculate_hash() #checking each block's hash
+            if(calc_hash != block.hash):  #if calculated hash does not match stored hash, return False
+                return False
+            if(calc_prev_hash != block.previous_block_hash): #checking each block's stored previous block hash with calculated previous block hash
+                return False
+            calc_prev_hash = calc_hash #storing in prev hash for next comparison (in next iteration)
+        return True
+
+
+    # Prints the data and hashes of each block in the chain
+    def print_chain(self):
+        # YOUR CODE HERE
+        for block in self.chain:
+            print("Data:", block.data, "Hash:", block.hash) #printing data and hash of each block in the block chain in a loop
+            print()
+
+    def store_chain_in_file(self):
+        file = open("blockchain.txt", "w")
+        file.write(str(self.difficulty))
+        file.write("\n")
+        for block in self.chain:
+            file.write(str(block.timestamp) + " " + str(block.nonce) + " " + str(block.data) + " " + str(block.previous_block_hash) + " " + str(block.hash))
+            file.write("\n")
+        file.close()
+
+    
+
+
+if __name__ == "__main__":
+
+    difficulty = 4
+    chain = Blockchain(difficulty, "")
+    chain.store_chain_in_file()
---
+++ netComm.py
@@ -0,0 +1,221 @@
+import socket
+from threading import Thread
+import random
+import rsa
+import json
+import blockchain
+import pickle
+
+
+
+class Client:
+
+    def __init__(self, pubKey, PrivKey):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.settimeout(None)
+        self.port = random.randint(10000, 40000)
+        self.sock.bind(('', self.port))
+        self.public_key = pubKey
+        self.private_key = PrivKey
+        self.money = 50
+
+    def del_from_file(self, itemName, quantity): #add new item or decrease old items quantity and write back updated dictionary
+        items_loaded = self.load_store()
+
+        if(itemName in items_loaded):
+            temp_tup = items_loaded[itemName]
+
+            final_quantity = temp_tup[1] - quantity
+            if(final_quantity <= 0):
+                del items_loaded[itemName]
+
+            else:
+                items_loaded[itemName] = (temp_tup[0], final_quantity, temp_tup[2])
+        
+        string_to_write = json.dumps(items_loaded)
+
+        file = open("Store.txt", "w")
+        file.write(string_to_write)
+
+
+    #reads dictionary of format {itemName:(publicKey, quantity, price) ...}
+    def load_store(self): #used to load the dictionary in Store.txt
+        file = open("Store.txt", "r")
+        items = file.read()
+        items = json.loads(items)
+
+        return items
+
+    #reads dictionary of format {portNumber:(N, E) ...}  
+    def load_keys(self): #loads key dictionary for use
+        file = open("keys.txt", "r")
+        keys = file.read()
+        keys = json.loads(keys)
+
+        return keys
+
+    def get_money_from_chain(self, pubKey):
+        # bc_file = open("blockchain.txt", "rb")
+        # chain = blockchain.Blockchain(4, "")
+        # chain = pickle.load(bc_file)
+        # bc_file.close()
+        chain = blockchain.open_chain_from_file()
+        if chain.is_chain_valid():
+            for block in chain:
+                transact = block.data.split()
+                if pubKey == transact[0]: #if sender
+                    self.money -= int(transact[2])
+                elif pubkey == transact[1]: #if receiver
+                    self.money += int(transact[2])
+            
+
+
+
+
+
+    def menu(self):
+
+        while True:
+
+            print('Enter 1 to see items available\nEnter 2 to sell an item\nEnter 3 to buy an item\n')
+
+            option = int(input())
+
+            if(option == 1): #displays all the items in the Store.txt file
+                file = open("Store.txt", "r")
+
+                item_info = file.read()  #reads dictionary of format {itemName:(publicKey, quantity, price) ...}
+
+                item_info = json.loads(item_info)
+
+                for key in item_info.keys():
+                    tup = item_info[key]
+                    print("Item name:", key, "quantity available:", tup[1], "price:", tup[2], "seller public key:", tup[0])
+
+                print("")
+            
+            elif(option == 2): #adds to the Store.txt file assuming two people don't sell same item
+
+                item = input("Enter the item name: ")
+                price = input("Enter item price: ")
+                quantity = input("Enter item quantity: ")
+
+                items_loaded = self.load_store()
+
+                if item in items_loaded: #if already exists only increase quantity
+                    temp_tup = items_loaded[item]
+
+                    items_loaded[item] = (temp_tup[0], temp_tup[1] + quantity, temp_tup[2])
+
+                else: #if doesn't exists create entry
+                    items_loaded[item] = ((self.public_key.n, self.public_key.e), quantity, price)
+
+                store = open("Store.txt", "w")
+                store.write(json.dumps(items_loaded))
+                store.close()
+
+
+            
+            elif(option == 3): #tells the seller of the transaction and adds it to the currently unconfirmed transactions list
+                
+
+                item = input("Enter item you want to buy: ")
+                quantity = input("Enter the quantity: ")
+
+                items_loaded = self.load_store()
+
+                if(not(item in items_loaded)): #if item does not exists
+                    print("That item is not found\n")
+                    continue
+
+                temp_tup = items_loaded[item]
+
+                item_price = temp_tup[2]
+                item_quantity = temp_tup[1]
+
+                money_in_wallet = self.get_money_from_chain(str((self.public_key.n, self.public_key.e)))
+
+                if(money_in_wallet >= (item_price * quantity)):
+                    #tell the seller you want to buy
+                    keys_loaded = self.load_keys()
+
+                    port_to_send = None
+
+                    for key in keys_loaded.keys():
+                        if (keys_loaded[key] == items_loaded[item][0]):
+                            port_to_send = key
+                            break
+
+                    message = "buy|" + item + "|" + quantity
+                    message = message.encode("utf-8")
+                    signature = rsa.sign(message, self.private_key, "SHA-1")
+
+                    self.sock.sendto(message +"|".encode("utf-8")+signature, ("localhost", port_to_send))
+                    
+                    #receive confirmation from seller
+
+                    #both use each other's public keys decode a message sent with private key to confirm identity
+
+                    #create data struct for blockchain or add to unconfirm transactions
+
+                    
+                    
+                    
+                    #update blockchain file
+                    chain = blockchain.open_chain_from_file()
+
+                    chain.add_block(blockchain.Block(str((self.public_key.n, self.public_key.e)) + " " + str(temp_tup[0]) + " " + str((item_price * quantity)), chain.get_latest_block().hash)) #add transaction in one block and add to blockchain after mining / showing proof of work
+                    #transaction format: sender receiver amount
+
+                    chain.store_chain_in_file()
+                    
+
+                    self.del_from_file(item, quantity) #update item's quantity in store
+                    
+
+
+                    #message seller item was sold
+
+
+    def receive_handler(self):
+        '''
+        Waits for a message and deals with it
+        '''
+        while True:
+
+            dataString, address = self.sock.recvfrom(4096)
+
+            print(dataString.decode("utf-8"))
+
+
+if __name__ == "__main__":
+
+    
+    # creats public private key pairs
+    # and adds the public key to the open list in keys.txt
+
+    (pubkey, privkey) = rsa.newkeys(256)  #creates public private key pair
+    user = Client(pubkey, privkey)
+
+    pubN = pubkey.n  #seperated as rsa.PublicKey is not JSON serialisable
+    pubE = pubkey.e
+
+    keys_loaded = user.load_keys()
+
+    keys_loaded[user.port] = (pubN, pubE)
+
+    file = open("keys.txt", "w")
+    keys_string = json.dumps(keys_loaded)
+    file.write(keys_string)
+    file.close()
+
+    #print(user.sock)
+    try:
+        # Start receiving Messages
+        T = Thread(target=user.receive_handler)
+        T.daemon = True
+        T.start()
+        # Start Client
+        user.menu() #add
+    except (KeyboardInterrupt, SystemExit):
+        exit()
